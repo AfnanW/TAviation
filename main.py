14dd1991a4c1d5e162ac30c8a372c8e2a26b8e3a
Afnan <waheed.afnan36@gmail.com>
2022-01-29 11:19:05 -0500

    Debug code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+#6d9a1d57ab714bf8fc6319165714b87950af510c
 def main_wrapper():
     print(f"function's name is {main_wrapper().__name__}")
+    print("hello")
+
 
 if __name__ == "__main__":
     main_wrapper()
