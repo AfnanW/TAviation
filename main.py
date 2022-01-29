583f50b85470b784f8fa3e2ff8b238017acd351c
Afnan <waheed.afnan36@gmail.com>
2022-01-29 11:45:06 -0500

    Code works!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #6d9a1d57ab714bf8fc6319165714b87950af510c
+#b8e781c3b109359a4b1f69e7becb64937ebbbdf3
+
 def main_wrapper():
-    print(f"function's name is {main_wrapper().__name__}")
+    print(f"function's name is {main_wrapper.__name__}")
     print("hello")
 
 
